49376ab5da12c7bb4b41b3a4eb01dbd609ace3ec
Safira13 <melanisafira2001@gmail.com>
2022-04-26 04:05:24 +0700

    portofolio

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+        <br></br>
+<hr></hr>
+
+<p>Copyright &copy; 2022 Melani safira.</p>
+    
+ 
+
+    </div>
+
+  );
+};
+
+export default Footer;
